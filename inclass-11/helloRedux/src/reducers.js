5ffbbfd6a5b7f0108c943ea33d557814692eb034
xun6000 <xun6000@gmail.com>
2017-02-20 15:07:43 -0600

    Add files via upload

---
+++ inclass-11/helloRedux/src/reducers.js
@@ -0,0 +1,50 @@
+//
+// Inclass Redux ToDo Exercise
+// ============================
+//
+// Your task is to implement the reducer below.
+//
+// Start this exercise by running the dev server:
+//    npm run dev
+// then navigate to the page and open the devtools console.
+// Try adding a new To Do item.  The console shows
+// the previous state, the action, and the new state.
+// 
+// Your task is to implement the reducer to properly
+// update the state.
+//
+// DO NOT USE MUTATION
+// there should be NO assignment operators in your solution.
+// 
+const Reducer = (state = {
+  nextId: 2,
+  todoItems: [
+    { id: 0, text: "This is an item", done: false },
+    { id: 1, text: "Another item", done: false }
+  ]
+}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state, nextId: state.nextId + 1,
+        todoItems: [...state.todoItems,
+        { id: state.nextId, text: action.text, done: false }]
+      }
+    case 'REMOVE_TODO':
+      return {
+        ...state,
+        todoItems: state.todoItems.filter(({id}) => id != action.id)
+      }
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todoItems: state.todoItems.map(({ id, text, done }) => (
+          { id, text, done: action.id === id ? !done : done }
+        ))
+      }
+    default:
+      return state
+  }
+}
+
+export default Reducer
